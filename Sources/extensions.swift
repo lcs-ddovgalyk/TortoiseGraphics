f8cc7c980ac0c39fc2e3cb6a6360223ac9d60e05
Dima Dovgalyk <ddovgalyk@lcs.on.ca>
2018-11-04 21:26:53 -0500

    Draws my full name + comments

--- Sources/extensions.swift
+++ Sources/extensions.swift
@@ -17,7 +17,7 @@ public extension Tortoise {
         self.penUp()
     }
     func upperD() {
-
+        //starts to draw the letter D
         self.penUp()
         self.forward(60)
         self.penDown()
@@ -25,14 +25,18 @@ public extension Tortoise {
         self.setHeading(190)
 
         self.penDown()
+        //makes the first curve for a capital D
         self.curve(withSides: 30, witchSize: 10, drawSides: 10)
         self.right(90)
+        //draws the side for the capital D
         self.curve(withSides: 30, witchSize: 10, drawSides: 9)
         self.left(30)
+        //Draws the top of the capital D
         self.curve(withSides: -60, witchSize: 5, drawSides: 10)
         self.curve(withSides: -29, witchSize: 15, drawSides: 15)
         self.curve(withSides: -30, witchSize: 5, drawSides: 10)
         self.curve(withSides: -15, witchSize: 10, drawSides: 7)
+        //moves to the side of the capital D so that the I can be drawn
         self.setHeading(90)
         self.penUp()
         self.forward(72)
@@ -41,35 +45,41 @@ public extension Tortoise {
 
     }
     func lowerI() {
+        //draws the letter I
         self.setHeading(25)
 
         self.penDown()
         self.forward(70)
         self.right(160)
         self.forward(80)
+        //connects the letter I to the next letter
         self.curve(withSides: 30, witchSize: 5, drawSides: 10)
         self.right(90)
         self.curve(withSides: 30, witchSize: 5, drawSides: 9)
 
     }
     func lowerM() {
+        //starts to draw the letter M
         self.setHeading(115)
         self.penDown()
         self.left(30)
         self.forward(10)
         self.left(65)
         self.forward(80)
+        //first part of the letter M
         self.curve(withSides: 50, witchSize: 2, drawSides: 24)
         self.penDown()
         self.forward(77)
         self.left(180)
         self.forward(67)
+        //second part of the letter M
         self.curve(withSides: 50, witchSize: 3, drawSides: 21)
         self.penDown()
         self.right(30)
         self.forward(70)
         self.right(180)
         self.forward(67)
+        //third part of the letter M
         self.curve(withSides: 50, witchSize: 3, drawSides: 21)
         self.penDown()
         self.right(30)
@@ -77,6 +87,7 @@ public extension Tortoise {
 
 }
     func lowerA() {
+        //Draws the half of the circle
         self.setHeading(40)
         self.penDown()
         self.forward(50)
@@ -90,16 +101,19 @@ public extension Tortoise {
         self.right(85)
         self.forward(57)
         self.left(90)
+        //draws the other halg of the cirle
         self.curve(withSides: -80, witchSize: 3, drawSides: 25)
         self.left(60)
         self.penDown()
         self.forward(77)
         self.right(180)
         self.forward(57)
+        //draws the ending of letter A so it would be able to connect to the next letter
         self.curve(withSides: -5, witchSize: 12, drawSides: 3)
 
     }
     func lowerO() {
+        //draws the letter O
         self.setHeading(10)
         self.curve(withSides: 50, witchSize: 10, drawSides: 7)
         self.right(180)
@@ -107,11 +121,14 @@ public extension Tortoise {
         self.left(20)
         self.curve(withSides: -10, witchSize: 30, drawSides: 4)
         self.left(100)
+        //connects the letter to the next letter
         self.curve(withSides: -10, witchSize: 11, drawSides: 4)
     }
     func lowerV() {
+        //draws the left side of the letter V
         self.setHeading(150)
         self.curve(withSides: 100, witchSize: 10, drawSides: 9)
+        //draws the right side of the letter V
         self.left(180)
         self.curve(withSides: 100, witchSize: 10, drawSides: 9)
 
@@ -122,6 +139,7 @@ public extension Tortoise {
         self.penDown()
         self.forward(15)
         self.setHeading(0)
+        //draws the top of the letter G
         self.curve(withSides: -20, witchSize: -13, drawSides: 10)
         self.right(180)
         self.penDown()
@@ -132,6 +150,7 @@ public extension Tortoise {
         self.curve(withSides: 20, witchSize: 13, drawSides: 10)
         self.penColor(.deepOrange)
         self.penDown()
+        //draws the down part of the letter G
         self.forward(150)
         self.curve(withSides: 20, witchSize: 13, drawSides: 10)
         self.right(40)
@@ -139,14 +158,18 @@ public extension Tortoise {
         self.forward(120)
     }
     func lowerL() {
+        //creates the curve of the letter L
         self.setHeading(45)
         self.curve(withSides: -60, witchSize: 18, drawSides: 7)
         self.curve(withSides: -10, witchSize: 10, drawSides: 5)
+        //drwas the letter L
         self.penDown()
         self.forward(110)
+        //draws the connecting part of the letter L to the next letter
         self.curve(withSides: -10, witchSize: 10, drawSides: 4)
     }
     func lowerY() {
+        //starts to draw the letter Y
         self.penDown()
         self.setHeading(20)
         self.forward(50)
@@ -156,25 +179,31 @@ public extension Tortoise {
         self.setHeading(0)
         self.right(180)
         self.curve(withSides: -18, witchSize: 13, drawSides: 9)
+        //draws the bottom part of the letter Y
         self.penDown()
         self.forward(20)
         self.backward(20)
         self.right(180)
         self.forward(100)
         self.curve(withSides: 20, witchSize: 13, drawSides: 10)
+        //connect the letter Y to the next letter
         self.right(40)
         self.penDown()
         self.forward(130)
     }
-    func lowerK() {
+    func lowerK(){
+        //draws the letter K
         self.setHeading(45)
+        //Draws the top of the letter K
         self.curve(withSides: -60, witchSize: 18, drawSides: 7)
         self.curve(withSides: -10, witchSize: 10, drawSides: 5)
+        //draws the top curve of the letter K
         self.penDown()
         self.forward(150)
         self.backward(50)
         self.setHeading(70)
         self.curve(withSides: -30, witchSize: 18, drawSides: 5)
+        //draws the bottom part of the letter K
         self.penDown()
         self.setHeading(45)
         self.backward(80)
